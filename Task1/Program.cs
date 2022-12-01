0282594664cb6248959b4f29042469bbfb87a815
kovalevandrey007 <kovalevandrey007@gmail.com>
2022-12-01 18:39:19 +0200

    2

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+
+*/
+
+
+int GetNumber(string message)
+{
+    int result = 0;
+    bool isCorrect = false;
+
+
+    while(!isCorrect)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result) && result >= 1)
+        {
+            isCorrect = true;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число или 0. Введите целое число больше 0");
+        }
+    }
+
+    return result;
+}
+
+int lin = GetNumber("Введите число");
+for(int i=1;i<=lin;i++)
+{
+    Console.Write($" {Math.Pow(i,2)}, ");
+}
